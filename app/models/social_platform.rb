86b5eb76bbaadada30067e055831279ba2731f88
marcon29 <mark@stablerwritingservices.com>
2020-07-12 02:52:48 -0600

    stubbed out social platform and profile models

--- app/models/social_platform.rb
+++ app/models/social_platform.rb
@@ -1,5 +1,61 @@
 class SocialPlatform < ActiveRecord::Base
-    has_many :social_profiles
+    # attrs/table setup: name, base_url, image_file
+
+
+    # associations:
+    has_many :social_profiles #destroy any social_profiles if a social_platform is deleted
+
+    
+    # attr validation:
+        # all must both exist and be unique
+        # image file must have .jpeg, .jpg, .png, .svg, .gif extension
+
+        # name
+        	# must exist, error message: "You must provide a name."
+		    # must be unique (case insensitive), error message: "That platform already exists. Please provide another."
+
+        # base_url
+            # must exist, error message: "You must provide the base URL."
+            # must be unique (case insensitive), error message: "That URL is already used for another platform. Please provide another."
+                # run format_base_url before saving
+            
+        # image_file
+            # must exist, error message: "You must provide the image file name."
+		    # must be unique (case insensitive), error message: "That image file already exists. Please provide another."
+		    # must have format, file extension must be one of: .jpeg .jpg .png .svg .gif, error message: "Only .jpeg, .jpg, .png, .svg, or .gif files are allowed."
+
+
+
+        # social full url examples: final trailing slash, caps and www. don't matter
+            # https://www.facebook.com/mark.stabler.984
+            # https://www.facebook.com/StablerWriter/
+            # https://twitter.com/stablerwriter/
+            # https://www.instagram.com/stablerwriter/
+            # https://www.linkedin.com/in/stablerwriter/
+            # https://www.reddit.com/user/stablerwriter
+            # https://github.com/marcon29
+
+
+
+	# helpers ################
+		# format_base_url
+            # takes user url entry, removes http and https, ensures www. is there, ensures trailing slash
+        
+        # image_file link 
+            # creates correct file path for image
+
+        # slug
+            # standard
+
+        # self.find_by_slug
+            # standard
+            
+
+
+	# view notes ################
+        # the image file needs to be loaded in the correct place
+            # add instructions on where it needs to go
+            #
 
 
 end
--- app/models/social_profile.rb
+++ app/models/social_profile.rb
@@ -1,5 +1,46 @@
-class SocialProfile < ActiveRecord::Base    
+class SocialProfile < ActiveRecord::Base
+    # attrs/table setup: name, handle, notes, social_platform_id
+
+
+    # associations:
     belongs_to :social_platform
+
+    
+    # attr validation:
+        # name and handle, they must both exist and be unique
+
+        # name 
+            # must exist, error message: "You must provide a name."
+		    # must be unique (case insensitive), error message: "That name is already used. Please provide another."
+		
+        # handle
+		    # must exist, error message: "You must provide your profile handle."
+            # must be unique (case insensitive), error message: "That handle is already used for that platform. Please provide another."
+                # uniqness is limited to the same platform
+            # run format_handle before saving
+		    
+
+
+	# helpers ################
+		# link
+            # creates link url from SocialPlatform.base_url + SocialProfile.handle
+        
+        # format_handle 
+            # removes @ if included
+
+        # slug
+            # standard slug, but should comprise of SocialPlatform.name and SocialProfile.name
+
+        # self.find_by_slug
+            # standard
+            
+
+            
+	# view notes ################
+		# add any notes based on validations and helpers so the proper data will appear in the view
+
+
+
 
 
 end
