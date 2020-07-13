938b9c898cebb5b50989512eb97486c677e4d27a
marcon29 <mark@stablerwritingservices.com>
2020-07-13 16:13:04 -0600

    AppController: stubbed out post contact route and tests

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -2,8 +2,8 @@ class AppController < Sinatra::Base
 	configure do
 		set :views, "app/views/"
 		set :public_folder, "public"
-		# enable :sessions
-		# set :session_secret, "mswoccp"
+		enable :sessions
+		set :session_secret, "mswoccp"
 	end
 
 	# home page
@@ -15,6 +15,48 @@ class AppController < Sinatra::Base
 	get '/contact' do
 		@contact_email = "online@markstabler.com"
 		erb :contact
+	end
+
+	post '/contact' do
+        # process contact form
+                
+		# if # email sent
+		# 	flash[:message] = "Your message was sent. I'll get back to you as soon as possible."
+        # else
+        #     flash[:message] = "There was a problem sending your message. Looks like I owe you a beer. Please submit again."
+		# end
+		
+		# redirect back
+    end
+
+
+
+	# app helpers ###############################
+	# add helpers used in numerous controllers
+
+	
+	
+	# model helpers ###############################
+	# add helpers used in specific controller
+
+
+
+	# user helpers ###############################
+	def login(user)
+		if user && user.authenticate(params[:user][:password])
+			session[:user_id] = user.id
+		else
+			# flash[:message] = "We did not find a matching profile. Please sign up instead."
+			redirect '/signup'
+		end		
+	end
+	
+	def current_user
+		User.find(session[:user_id])
+	end
+
+	def logged_in?
+		!!session[:user_id]
 	end
 
 
--- spec/controllers/app_controller_spec.rb
+++ spec/controllers/app_controller_spec.rb
@@ -11,7 +11,61 @@ describe "AppController" do
 		# expect(test value).to eq("known true value")
 	# end
 
-	# repeate for each functionality to be tested
+    # repeate for each functionality to be tested
+    
+    describe "Home page" do
+        it "loads the homepage" do
+            # visit '/'
+
+            # expect(page.status_code).to eq(200)
+            # expect(page.body).to include("")
+        end
+
+        it "displays all content_sections" do
+            # visit '/'
+
+            # ContentSection.all.count
+            # expect number of sections to equal the count
+            # expect(page.body).to include("")
+        end
+
+        it "links to contact page" do
+            # visit '/'
+
+            # expect(page.body).to include("")
+        end
+    end
+
+    describe "Contact page" do
+        it "loads the contact page and displays the contact form" do
+            visit '/contact'
+            
+            # expect(page.status_code).to eq(200)
+            # expect(page.body).to include("")
+        end
+    
+        it "lets a user create and send an email to the correct address, then reloads the page" do
+            # visit '/contact'
+            
+            # fill_in :first_name, :with => "input value"
+            # fill_in :last_name, :with => "input value"
+            # fill_in :email, :with => "input value"
+            # fill_in :subject, :with => "input value"
+            # fill_in :message, :with => "input value"
+            # click_button "Submit"            
+
+            # expect(params[:first_name]).to eq("input value")
+            # expect(params[:last_name]).to eq("input value")
+            # expect(params[:email]).to eq("input value")
+            # expect(params[:subject]).to eq("input value")
+            # expect(params[:message]).to eq("input value")
+            # expect(params[:message]).to eq("input value")
+            
+            # # need to verify email address sent to and how info is transfered for mailto form
+
+            # expect(page).to have_current_path("/contact")
+        end
+    end
 
 
 	# helper method tests ########################################################
